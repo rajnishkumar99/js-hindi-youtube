d0723176568d9e4d20f676e97c0120dea6fed837
Rajnish Kumar <141429495+rajnishkumar99@users.noreply.github.com>
2023-08-05 22:10:53 +0000

    lecture 2 data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) //we are using node.js, not browser
+
+console.log(3 
+    +
+     3); //not prefer because code readability should be high
+
+
+let name = "rajnish"
+let age = 18
+let isLoggedIn = false
+let state;
+/* primitive data type */
+
+// number range => 2 to power 53
+// bigint => using for wide range of number
+// string =>"" 
+// boolean => true/false
+// null => standalone value (empty value)
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null) //object
