1365910b4cd56a15b78377e722fd6041dcd2d99c
Rajnish Kumar <141429495+rajnishkumar99@users.noreply.github.com>
2023-08-05 21:19:16 +0000

    first lecutre

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144
+let accountEmail = "aghorirajnish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "razz@gamil.com"
+accountPassword = "565656"
+accountCity = "Bihar"
+let accountState
+
+// accountId = 55    //not allowed
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
