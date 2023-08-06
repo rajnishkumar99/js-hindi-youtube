9d991d710376836cad21088e6e1e398e7c0e4e84
Rajnish Kumar <141429495+rajnishkumar99@users.noreply.github.com>
2023-08-06 15:30:55 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+null => 0
+undefined => NaN
+true => 1; 
+false => 0;
+*/
+
+let isLoggedIn = "raaz";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/*[In Boolean Conversion]
+1 => true;
+0 => false;
+"" => false;
+"rajnish" => true;
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
